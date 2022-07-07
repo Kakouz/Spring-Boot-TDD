c674251d8c220b63584f5c7a6c6f9f2d35b41376
Tiago <tf.prudente@gmail.com>
2022-07-07 18:20:41 -0300

    Test Endpoint update User Resource

--- src/test/java/br/com/tdd/api/resources/UserResourceTest.java
+++ src/test/java/br/com/tdd/api/resources/UserResourceTest.java
@@ -110,7 +110,22 @@ class UserResourceTest {
     }
 
     @Test
-    void update() {
+    void whenUpdatingUserReturnSuccess() {
+        when(userService.update(any())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = userResource.update(ID, userDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+
     }
 
     @Test

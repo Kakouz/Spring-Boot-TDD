ca41f99c365b1436dd0d304fc99f1cbd007534b9
Tiago <tf.prudente@gmail.com>
2022-07-07 11:34:52 -0300

    Endpoint Find All Users

--- src/main/java/br/com/tdd/api/resources/UserResource.java
+++ src/main/java/br/com/tdd/api/resources/UserResource.java
@@ -1,9 +1,7 @@
 package br.com.tdd.api.resources;
 
-import br.com.tdd.api.domain.User;
 import br.com.tdd.api.domain.dto.UserDTO;
 import br.com.tdd.api.services.UserService;
-import org.apache.catalina.mapper.Mapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -25,5 +25,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<UserDTO> listDTO = userService.findAll().stream()
+                .map(x -> mapper.map(x, UserDTO.class))
+                .toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/br/com/tdd/api/services/UserService.java
+++ src/main/java/br/com/tdd/api/services/UserService.java
@@ -3,7 +3,10 @@ package br.com.tdd.api.services;
 
 import br.com.tdd.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById (Integer id);
+    List<User> findAll();
 }

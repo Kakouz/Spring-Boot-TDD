4ca5c7876d8289825eec58de5f22137991285210
Tiago <tf.prudente@gmail.com>
2022-07-07 11:10:41 -0300

    Tratamento de exceções, User Not Found

--- src/main/java/br/com/tdd/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/tdd/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.tdd.api.services.impl;
 import br.com.tdd.api.domain.User;
 import br.com.tdd.api.repositories.UserRepository;
 import br.com.tdd.api.services.UserService;
+import br.com.tdd.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> user = userRepository.findById(id);
-        return user.orElse(null);
+        return user.orElseThrow(() -> new ObjectNotFoundException("User not found!"));
     }
 }

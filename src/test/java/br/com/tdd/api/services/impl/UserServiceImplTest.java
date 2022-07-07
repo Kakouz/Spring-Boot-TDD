2aa16c4bdae52e0f7b5cc665724af80b8d1e5eae
Tiago <tf.prudente@gmail.com>
2022-07-07 16:43:40 -0300

    Test User Service Find By Id

---
+++ src/test/java/br/com/tdd/api/services/impl/UserServiceImplTest.java
@@ -0,0 +1,92 @@
+package br.com.tdd.api.services.impl;
+
+import br.com.tdd.api.domain.User;
+import br.com.tdd.api.domain.dto.UserDTO;
+import br.com.tdd.api.repositories.UserRepository;
+import br.com.tdd.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    public static final Integer ID      = 1;
+    public static final String NAME     = "Tiago";
+    public static final String EMAIL    = "tiago@email.com";
+    public static final String PASSWORD = "123";
+
+    @InjectMocks
+    private UserServiceImpl service;
+
+    @Mock
+    private UserRepository repository;
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> userOptional;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnUserInstance() {
+        Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(userOptional);
+
+        User response = service.findById(ID);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(User.class, response.getClass());
+        Assertions.assertEquals(ID, response.getId());
+        Assertions.assertEquals(NAME, response.getName());
+        Assertions.assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void whenFindByIdThenReturnObjectNotFoundException() {
+        Mockito.when(repository.findById(Mockito.anyInt()))
+                .thenThrow(new ObjectNotFoundException("User not found!"));
+        try {
+            service.findById(ID);
+        } catch (Exception ex) {
+            Assertions.assertEquals(ObjectNotFoundException.class, ex.getClass());
+            Assertions.assertEquals("User not found!", ex.getMessage());
+        }
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        userOptional = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+    }
+
+}

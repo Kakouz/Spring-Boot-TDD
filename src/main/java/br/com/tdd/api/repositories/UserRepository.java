1b6119b7a9aff5eeb7926151772d090d7a266180
Tiago <tf.prudente@gmail.com>
2022-07-07 10:59:18 -0300

    Interface User Repository Find by id

---
+++ src/main/java/br/com/tdd/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.tdd.api.repositories;
+
+import br.com.tdd.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
